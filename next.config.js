c3d3b44c4f1af1af7362d38df6b4b033726e0f5f
gracedward <edward.chung.designer@gmail.com>
2023-10-25 08:41:50 +0100

    fix: multiple files on vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoader, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
